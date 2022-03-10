17d5fd682a646445f62f6bf005c15eec058f32cf
rpo <pozzir00@gmail.com>
2022-03-10 11:39:09 +0100

    Install script in path

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+if [ -z "$1" ]
+then
+    destpath="~/.local/bin"
+else
+    destpath="$1"
+fi
+
 echo "Insert your TELEGRAM_BOT_TOKEN."
 read -s yourtoken
 echo ""
@@ -12,3 +19,12 @@ export yourchatid
 
 mkdir -p ~/.local/share/teleclinotify
 envsubst < config.sample > ~/.local/share/teleclinotify/config
+
+echo "Installing teleclinotify into ${destpath}..."
+if ! cp teleclinotify $destpath 2>/dev/null
+then
+    echo "Error intalling teleclinotify into ${destpath}."
+    echo "You may not have writing permission to ${destpath}."
+    echo "Trying with sudo; CTRL+C to exit."
+    sudo cp teleclinotify $destpath 2>/dev/null || echo "Failed."
+fi
